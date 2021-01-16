3effec8afa83192d07910e31397ed8d563e9c3e8
Natthawut Haematulin <natthawut1991@gmail.com>
2021-01-16 08:49:26 +0700

    feat: implement reload table view after saved note by using segue

--- SimpleNoteMVC/SimpleNote/NoteFormViewController.swift
+++ SimpleNoteMVC/SimpleNote/NoteFormViewController.swift
@@ -7,23 +7,23 @@
 
 import UIKit
 
-class NoteFormViewController: UIViewController {
-
+class NoteFormViewController: UIViewController, UITextFieldDelegate {
+    
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var noteTextField: UITextField!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
     
     var delegate: NoteFormDelegate?
-    
+    var note: Note?
     private let noteService = NoteService.shared
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
     @IBAction func saveNote(_ sender: UIBarButtonItem) {
-        let note = Note(
+        note = Note(
             id:Int.random(in: 1..<1000),
             title: titleTextField.text ?? "",
             note: noteTextField.text ?? "",
@@ -31,7 +31,7 @@ class NoteFormViewController: UIViewController {
             image: "",
             createdAt: Date()
         )
-        noteService.addNote(note)
+        noteService.addNote(note!)
         delegate?.noteFormSaved()
         dismiss(animated: true, completion: nil)
     }
@@ -41,16 +41,35 @@ class NoteFormViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
-    /*
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == titleTextField {
+            
+        }
+    }
+    
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        super.prepare(for: segue, sender: sender)
+        
+        guard let button = sender as? UIBarButtonItem, button == saveButton else {
+            print("The save button was not pressed, cancelling")
+            return
+        }
+        
+        note = Note(
+            id:Int.random(in: 1..<1000),
+            title: titleTextField.text ?? "",
+            note: noteTextField.text ?? "",
+            author: "",
+            image: "",
+            createdAt: Date()
+        )
+        
+        noteService.addNote(note!)
     }
-    */
-
+    
 }
 
 protocol NoteFormDelegate {
