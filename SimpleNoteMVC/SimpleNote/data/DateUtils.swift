5003c8bd59f82a599761bbb7bb7ff4aad4247a7f
Natthawut Haematulin <natthawut1991@gmail.com>
2021-01-24 15:45:41 +0700

    feat: implement add, update, delete note

---
+++ SimpleNoteMVC/SimpleNote/data/DateUtils.swift
@@ -0,0 +1,21 @@
+//
+//  DateFormatterUtils.swift
+//  SimpleNoteMVC
+//
+//  Created by Natthawut Haematulin on 24/1/2564 BE.
+//
+
+import Foundation
+
+var dateFormatter: DateFormatter {
+    get {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        // Use format appropriate to your JSON String.  This is for ISO-8601
+        // You MUST account for the milliseconds even if you don't want them
+        // or it won't parse properly
+        dateFormatter.dateFormat = "yy-MM-dd'T'HH:mm:ssZ"
+        return dateFormatter
+    }
+}
+    
---
+++ SimpleNoteMVC/SimpleNote/data/Parameters.swift
@@ -0,0 +1,12 @@
+//
+//  Parameters.swift
+//  SimpleNoteMVC
+//
+//  Created by Natthawut Haematulin on 24/1/2564 BE.
+//
+
+import Foundation
+
+protocol Parameters: Encodable {
+    
+}
